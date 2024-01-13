b9967dc7d33a9e6c8fdd8aee33a9ef594d466ae9
sarah vieira <sarahvieirads18@gmail.com>
2024-01-12 23:42:18 -0300

    carousel js - section 4

---
+++ js/carousel-slick.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".img_amazonia").slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+        focusOnSelect: true,
+        centerMode: true,
+        adaptiveHeight: true,
+    })
+})
